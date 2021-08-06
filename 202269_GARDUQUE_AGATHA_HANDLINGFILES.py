19590ee391f216fbdd5cb78c7bbcc036b9123ea8
agafei <86621671+agafei@users.noreply.github.com>
2021-08-06 21:07:23 +0800

    Update 202269_GARDUQUE_AGATHA_HANDLINGFILES.py

--- 202269_GARDUQUE_AGATHA_HANDLINGFILES.py
+++ 202269_GARDUQUE_AGATHA_HANDLINGFILES.py
@@ -18,43 +18,44 @@ def get_property(code,property):
     return float(products[code][property])
 
 
+
 def main():
-    with open('receipt01.txt','a+') as f:
+    with open("receipt01.txt","a+") as f:
         total=0
         y=0
         final={}
-        f.write('\n==\n')
-        f.write('CODE\t\t\tNAME\t\t\tQUANTITY\t\t\tSUBTOTAL\n')
-
+        f.write("\n==\n")
+        f.write("CODE\t\t\tNAME\t\t\tQUANTITY\t\t\tSUBTOTAL\n")
+        
         while True:
-            order=input('{product_code},{quantity}: ')
-            if order!='/':
-                code=order.split(',')[0]
-                quantity=order.split(',')[1]
-                subtotal=get_property(code,'price')*float(quantity)
+            order=input("{product_code},{quantity}: ")
+            if order!="/":
+                code=order.split(",")[0]
+                quantity=int(order.split(",")[1])
+                subtotal=get_property(code,"price")*int(quantity)
                 check=code in final
-                total=total+subtotal
+                total+=subtotal
                 if check==True:
-                    final[code]['quantity']+=quantity
-                    final[code]['subtotal']+=subtotal
+                    final[code]["quantity"]+=quantity
+                    final[code]["subtotal"]+=subtotal     
                 else:
                     final[code]=get_product(code)
-                    final[code]['quantity']=quantity
-                    final[code]['subtotal']=subtotal
+                    final[code]["quantity"]=quantity
+                    final[code]["subtotal"]=subtotal
             else:
                 sorted_final=sorted(final)
-
+                
                 while y<len(final):
-                    f.write(sorted_final[y]+'\t\t')
-                    if len(list(final[sorted_final[y]]['name']))>8:
-                        f.write(final[sorted_final[y]]['name']+'\t\t')
-                    else:
-                        f.write('\t'+final[sorted_final[y]]['name']+'\t\t\t')
-                    f.write(str(final[sorted_final[y]]['quantity'])+'\t\t\t')
-                    f.write(str(final[sorted_final[y]]['subtotal'])+'\n')
+                    f.write(sorted_final[y]+"\t\t")
+                    if len(list(final[sorted_final[y]]["name"]))>8:
+                        f.write(final[sorted_final[y]]["name"]+"\t\t")
+                    else:  
+                        f.write("\t"+final[sorted_final[y]]["name"]+"\t\t\t")
+                    f.write(str(final[sorted_final[y]]["quantity"])+"\t\t\t")
+                    f.write(str(final[sorted_final[y]]["subtotal"])+"\n")
                     y+=1
-                f.write('\nTotal:\t\t\t\t\t\t\t\t\t'+str(total))
-                f.write('\n==')
+                f.write("\nTotal:\t\t\t\t\t\t\t\t\t"+str(total))
+                f.write("\n==")
                 f.close()
                 break
 main()
